7632c3ccd0f8986ef3ebcf749c8b343ea468dee4
Particle4Dev <steve@p4d-Vostro-270.(none)>
2014-03-04 20:37:05 +0700

    more test

--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,17 @@
 Tinytest.add("sass - presence", function(test) {
 
   var d = OnscreenDiv(Meteor.render(function() {
-    return '<p class="test-class"></p>'; }));
+    return '<p class="test-class"><b class="example"></b></p>'; }));
   d.node().style.display = 'block';
 
   var p = d.node().firstChild;
   test.equal(getStyleProperty(p, 'font'), "normal normal normal 14px/normal Helvetica, sans-serif");
 
-  // test @import
   test.equal(getStyleProperty(p, 'color'), "rgb(204, 204, 204)");
+
+  // test @import
+  var i = d.div.getElementsByClassName('example');
+  test.equal(getStyleProperty(i[0], 'color'), "rgb(255, 0, 0)");
 
   d.kill();
 });
