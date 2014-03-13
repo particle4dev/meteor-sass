3b6b6aea541e2ad0abd0c5940d45e8582f103f30
Particle4Dev <steve@p4d-Vostro-270.(none)>
2014-03-13 08:55:09 +0700

    Update node-sass to version 0.8.3

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package._transitional_registerBuildPlugin({
   sources: [
     'plugin/compile-sass.js'
   ],
-  npmDependencies: {"node-sass": "0.8.1"}
+  npmDependencies: {"node-sass": "0.8.3"}
 });
 
 Package.on_test(function (api) {
