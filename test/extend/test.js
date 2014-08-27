62abe7d47a524a0157f843a61ef67b082f234325
Particle4Dev <steve@p4d-Vostro-270.(none)>
2014-08-28 06:01:37 +0700

    update

--- test/extend/test.js
+++ test/extend/test.js
@@ -2,13 +2,11 @@ Tinytest.add("sass - extend", function(test) {
   var d = OnscreenDiv(Template.extend);
   d.node().css('display', 'block');
 
-  var message = d.node().find('.messageExtends');
-  test.equal(message.css('border'), "1px solid rgb(204, 204, 204)");
-  test.equal(message.css('padding'), "10px");
-  test.equal(message.css('color'), "rgb(51, 51, 51)");
-
   var success = d.node().find('.successExtends');
   test.equal(success.css('border-color'), "rgb(0, 128, 0)", 'success fail');
+  test.equal(success.css('border'), "1px solid rgb(0, 128, 0)");
+  test.equal(success.css('padding'), "10px");
+  test.equal(success.css('color'), "rgb(51, 51, 51)");
 
   var error = d.node().find('.errorExtends');
   test.equal(error.css('border-color'), "rgb(255, 0, 0)", 'error fail');
